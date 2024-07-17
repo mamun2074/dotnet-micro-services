9697aa5e57a96c9964fb2cd7953fa2527013baf3
Mahmud <al.mahmud@softrobotics.com.bd>
2024-07-17 13:29:44 +0600

    MediatR library include,  Create product, Comand,  result added on create product handler but not implemented

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductComand(string Name, List<string> Category, string Description, string imageFile, decimal Price) : IRequest<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductComand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductComand request, CancellationToken cancellationToken)
+    {
+        //Business logic to create a product
+        throw new NotImplementedException();
+    }
+}
