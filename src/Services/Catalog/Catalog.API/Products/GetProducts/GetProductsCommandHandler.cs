cddbef76679963f44b804defd1769ed7fbc96be8
Mahmud <al.mahmud@softrobotics.com.bd>
2024-08-17 11:33:06 +0600

    Get products

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsCommandHandler.cs
@@ -1,8 +1,15 @@
-﻿namespace Catalog.API.Products.GetProducts;
+﻿
+namespace Catalog.API.Products.GetProducts;
 
 public record GetProductsQuery() : IQuery<GetProductsResult>;
 public record GetProductsResult(IEnumerable<Product> Products);
 
-public class GetProductsCommandHandler
+internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductsQueryHandler called {@query}", query);
+        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        return new GetProductsResult(products);
+    }
 }
