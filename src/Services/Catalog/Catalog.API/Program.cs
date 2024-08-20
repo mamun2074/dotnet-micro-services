6b5fcc80910e0a7eb8d5cea03ade7d5eb605726c
Mahmud <al.mahmud@softrobotics.com.bd>
2024-08-20 15:40:04 +0600

    Seeder added

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -20,6 +20,11 @@ builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
+// seeding condition
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
+}
 
 // add custome exception handler
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
