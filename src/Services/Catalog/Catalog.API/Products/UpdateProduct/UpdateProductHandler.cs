af487be16387e9e09b80d8d936458832f62beb57
Mahmud <al.mahmud@softrobotics.com.bd>
2024-08-20 11:34:50 +0600

    Not found, Bad request,Internal Server exception class create on building block and implement it on product not found exception.

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -22,7 +22,7 @@ internal class UpdateProductHandler(IDocumentSession session) : IComandHandler<U
         var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
         if (product is null)
         {
-            throw new ProductNotFoundException();
+            throw new ProductNotFoundException(command.Id);
         }
         product.Name = command.Name;
         product.Category = command.Category;
