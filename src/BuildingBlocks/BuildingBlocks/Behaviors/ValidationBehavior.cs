274f52e9f441cadc1f7721482c981d77dfe3acc4
Mahmud <al.mahmud@softrobotics.com.bd>
2024-08-20 19:37:28 +0600

    Catalog Api completed.

--- src/BuildingBlocks/BuildingBlocks/Behaviors/ValidationBehavior.cs
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/ValidationBehavior.cs
@@ -14,7 +14,6 @@ public class ValidationBehavior<TRequest, TResponse>
     {
         var context = new ValidationContext<TRequest>(request);
         var validationResults = await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken)));
-
         var failures = validationResults.Where(r => r.Errors.Any())
             .SelectMany(r => r.Errors)
             .ToList();
